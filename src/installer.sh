370d19fbc7c1ad284346daa8cb2c9751895eb123
ljp-projects <lucasplumb@icloud.com>
2024-05-30 07:26:27 +1000

    Fix bug where standard library for non-native js functions 'not_supported_js' needed escape or double quotes

--- src/installer.sh
+++ src/installer.sh
@@ -2,14 +2,22 @@ tea_version="v1.0.0-beta.1"
 
 cd /tmp/ || exit
 git clone https://github.com/ljp-projects/TeaScript.git
+
 cd TeaScript || exit
-unzip src/TeaScript.jar.zip /usr/local/bin/TeaScript.jar
+
+unzip src/TeaScript.jar.zip src/TeaScript.jar
+cp src/TeaScript.jar /usr/local/bin/TeaScript.jar
+
 touch tea
 echo "java -jar /usr/local/bin/TeaScript.jar \$*" > tea
+
 touch tea-version
 echo "$tea_version" > tea-version
+
 chmod +x tea
+
 mv tea /usr/local/bin
 mv tea-version /usr/local/bin
+
 cd ..
 rm -rf TeaScript
