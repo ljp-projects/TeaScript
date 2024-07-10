6052a9421eca8ad466d30a8b2ebd60b7bdc637b4
ljp-projects <lucasplumb@icloud.com>
2024-07-10 23:21:03 +1000

    Update installer.sh to use new system

--- src/installer.sh
+++ src/installer.sh
@@ -1,12 +1,7 @@
 tea_version=$(curl  "https://api.github.com/repos/ljp-projects/teascript/tags" | jq -r '.[0].name')
 
-cd /tmp/ || exit
-git clone https://github.com/ljp-projects/TeaScript.git
-
-cd TeaScript || exit
-
-xz -d -T8 src/TeaScript.jar.xz
-cp src/TeaScript.jar /usr/local/bin/TeaScript.jar
+cd /usr/local/bin/ || exit
+mv "$1" ./
 
 touch tea
 echo "java -jar /usr/local/bin/TeaScript.jar \$*" > tea
@@ -15,9 +10,3 @@ touch tea-version
 echo "$tea_version" > tea-version
 
 chmod +x tea
-
-mv tea /usr/local/bin
-mv tea-version /usr/local/bin
-
-cd ..
-rm -rf TeaScript
